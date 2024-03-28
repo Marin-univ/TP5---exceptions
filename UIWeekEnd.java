4a87132d72c1d206c59b3fe226a6682bd1fcd390
Marin-univ <144822096+Marin-univ@users.noreply.github.com>
2024-03-28 08:28:45 +0100

    Sauv m (#1)
    * Menu
    
    * changement aurevoir
    
    ---------
    
    Co-authored-by: Marin Chesneau <marin.chsn@gmail.com>

--- UIWeekEnd.java
+++ UIWeekEnd.java
@@ -28,8 +28,15 @@ class AppWeekEnd {
     public void menu() {
 	boolean commande_faite = false;
 	while(!commande_faite) {
-	    System.out.println("Que voulez vous faire?");
-	    System.out.println("Q: quitter");
+		System.out.println("╭─────────────────────╮");
+		System.out.println("│ Menu Principale     │");
+		System.out.println("|─────────────────────|");
+		System.out.println("│ Q: quitter          │");
+		System.out.println("│ D: Menu dépenses    │");
+		System.out.println("│ P: Menu personnes   │");
+		System.out.println("│ T: Total dépenses   │");
+		System.out.println("│ M: Moyenne dépenses │");
+		System.out.println("╰─────────────────────╯");
 	    String commande_brute = System.console().readLine();
 	    String commande = commande_brute.strip().toLowerCase();
 	    if(commande.equals("q")) {
@@ -56,16 +63,17 @@ class AppWeekEnd {
 
     /// Affiche un message de bienvenue
     public void bienvenue() {
-	System.out.println("╭────────────────────────────────────────────────────────────────────────────────────╮");
-	System.out.println("│ Bienvenue! En week-end comme dans la semaine, les bons comptes font les bons amis. │");
-	System.out.println("╰────────────────────────────────────────────────────────────────────────────────────╯");
+		System.out.println("╭────────────────────────────────────────────────────────────────────────────────────╮");
+		System.out.println("│ Bienvenue! En week-end comme dans la semaine, les bons comptes font les bons amis. │");
+		System.out.println("╰────────────────────────────────────────────────────────────────────────────────────╯");
     }
 
     /// Affiche un message d'au revoir
     public void au_revoir() {
-		System.out.println("╭────────────────────────────────────────────────────────────────────────────────────╮");
-		System.out.println("│ Au Revoir! En week-end comme dans la semaine, les bons comptes font les bons amis. │");
-		System.out.println("╰────────────────────────────────────────────────────────────────────────────────────╯");
+		System.out.println("╭────────────╮");
+		System.out.println("│ Au Revoir! │");
+		System.out.println("╰────────────╯");
     }
 
 }
+
